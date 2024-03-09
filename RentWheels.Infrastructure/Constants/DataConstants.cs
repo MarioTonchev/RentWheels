93be754fc1fad93c7aaff37f1844a590c9ea7b98
MarioTonchev <mariotonchev500@gmail.com>
2024-03-09 20:39:12 +0200

    Added car data constants

--- RentWheels.Infrastructure/Constants/DataConstants.cs
+++ RentWheels.Infrastructure/Constants/DataConstants.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace RentWheels.Infrastructure.Constants
 {
-	public class DataConstants
+	public static class DataConstants
 	{
+		//Car
+		public const int CarMinBrandLength = 2;
+		public const int CarMaxBrandLength = 30;
+
+		public const int CarMinModelLength = 1;
+		public const int CarMaxModelLength = 30;
+
+		public const int CarMinColorLength = 3;
+		public const int CarMaxColorLength = 25;
+
+		public const double CarMinPricePerDay = 0.00;
+		public const double CarMaxPricePerDay = 2000.00;
 	}
 }
