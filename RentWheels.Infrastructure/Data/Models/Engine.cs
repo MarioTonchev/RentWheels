bef07529119a6940bac37c5d279ac2a3622de08b
MarioTonchev <mariotonchev500@gmail.com>
2024-03-09 21:02:10 +0200

    Car and Engine entities created

---
+++ RentWheels.Infrastructure/Data/Models/Engine.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentWheels.Infrastructure.Data.Models
+{
+	public class Engine
+	{
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Horsepower { get; set; }
+        [Required]
+        public int Cubage { get; set; }
+        [Required]
+        public string FuelType { get; set; } = string.Empty;
+    }
+}
