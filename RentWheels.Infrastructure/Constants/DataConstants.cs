636d4e7ad58f2571f205df6af36329e76db18008
MarioTonchev <mariotonchev500@gmail.com>
2024-03-10 17:14:47 +0200

    Modified DataConstants.cs

--- RentWheels.Infrastructure/Constants/DataConstants.cs
+++ RentWheels.Infrastructure/Constants/DataConstants.cs
@@ -8,17 +8,30 @@ namespace RentWheels.Infrastructure.Constants
 {
 	public static class DataConstants
 	{
-		//Car
-		public const int CarMinBrandLength = 2;
-		public const int CarMaxBrandLength = 30;
+		public static class CarConstants
+		{
+			public const int CarMinBrandLength = 2;
+			public const int CarMaxBrandLength = 30;
 
-		public const int CarMinModelLength = 1;
-		public const int CarMaxModelLength = 30;
+			public const int CarMinModelLength = 1;
+			public const int CarMaxModelLength = 30;
 
-		public const int CarMinColorLength = 3;
-		public const int CarMaxColorLength = 25;
+			public const int CarMinColorLength = 3;
+			public const int CarMaxColorLength = 25;
 
-		public const double CarMinPricePerDay = 0.00;
-		public const double CarMaxPricePerDay = 2000.00;
+			public const double CarMinPricePerDay = 0.00;
+			public const double CarMaxPricePerDay = 2000.00;
+		}
+
+		public static class ReviewConstants
+		{
+			public const int ReviewMinCommentLength = 15;
+			public const int ReviewMaxCommentLength = 1500;
+
+			public const int ReviewMinRating = 1;
+			public const int ReviewMaxRating = 10;
+		}
+
+		public const string DateFormat = "dd/MM/yyyy";
 	}
 }
