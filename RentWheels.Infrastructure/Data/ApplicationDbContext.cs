8104f0018bbaccac2943728c22c9934d5e7e303d
MarioTonchev <mariotonchev500@gmail.com>
2024-03-10 17:15:15 +0200

    Moved the Models folder

--- RentWheels.Infrastructure/Data/ApplicationDbContext.cs
+++ RentWheels.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RentWheels.Infrastructure.Models;
 
 namespace RentWheels.Infrastructure.Data
 {
-	public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext
 	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 			: base(options)
 		{
 		}
-	}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.Entity<RentalLocation>().HasKey(rl => new { rl.RentalId, rl.LocationId });
+
+			base.OnModelCreating(builder);
+		}
+
+		public DbSet<Car> Cars { get; set; }
+        public DbSet<Engine> Engines { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<RentalLocation> RentalsLocations { get; set; }
+    }
 }
