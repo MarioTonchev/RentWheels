51948232171129e8484d6979fa4a9634c397e636
MarioTonchev <mariotonchev500@gmail.com>
2024-03-12 01:22:38 +0200

    Implemented the Car Service

--- RentWheels.Core/Contracts/ICarService.cs
+++ RentWheels.Core/Contracts/ICarService.cs
@@ -5,11 +5,12 @@ namespace RentWheels.Core.Contracts
 {
 	public interface ICarService
 	{
-		Task<IEnumerable<CarAllViewModel>> AllAsync();
+		Task<IEnumerable<CarAllViewModel>> AllCarsAsync();
+		Task<IEnumerable<CarAllViewModel>> LastThreeCarsAsync();
 		Task<IEnumerable<EngineAllViewModel>> AllEnginesAsyc();
 		Task<bool> CarExistsAsync(int carId);
 		Task<bool> EngineExistsAsync(int engineId);
-		Task<int> AddAsync(CarAddViewModel model);
+		Task<int> CreateAsync(CarAddViewModel model, string ownerId);
 		Task<CarDetailsViewModel> DetailsAsync(int carId);
 	}
 }
--- RentWheels.Core/Services/CarService.cs
+++ RentWheels.Core/Services/CarService.cs
@@ -1,39 +1,87 @@
-﻿using RentWheels.Core.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using RentWheels.Core.Contracts;
 using RentWheels.Core.VeiwModels.Car;
 using RentWheels.Core.VeiwModels.Engine;
+using RentWheels.Infrastructure.Common;
+using RentWheels.Infrastructure.Models;
 
 namespace RentWheels.Core.Services
 {
 	public class CarService : ICarService
 	{
-		public Task<int> AddAsync(CarAddViewModel model)
+		private readonly IRepository repository;
+
+        public CarService(IRepository _repository)
+        {
+			repository = _repository;
+        }
+
+		public async Task<IEnumerable<CarAllViewModel>> AllCarsAsync()
 		{
-			throw new NotImplementedException();
+			return await repository.AllAsReadOnly<Car>().Select(c => new CarAllViewModel()
+			{
+				Brand = c.Brand,
+				Model = c.Model,
+				Year = c.Year,
+				ImageUrl = c.ImageUrl
+			}).ToListAsync();
 		}
 
-		public Task<IEnumerable<CarAllViewModel>> AllAsync()
+        public async Task<IEnumerable<CarAllViewModel>> LastThreeCarsAsync()
+        {
+            return await repository.AllAsReadOnly<Car>().OrderByDescending(c => c.Id)
+				.Select(c => new CarAllViewModel()
+            {
+                Brand = c.Brand,
+                Model = c.Model,
+                Year = c.Year,
+                ImageUrl = c.ImageUrl
+            }).Take(3).ToListAsync();
+        }
+
+		public async Task<IEnumerable<EngineAllViewModel>> AllEnginesAsyc()
 		{
-			throw new NotImplementedException();
+			return await repository.AllAsReadOnly<Engine>().Select(e => new EngineAllViewModel()
+			{
+				HorsePower = e.Horsepower,
+				Cubage = e.Cubage,
+				FuelType = e.FuelType,
+			}).ToListAsync();
 		}
 
-		public Task<IEnumerable<EngineAllViewModel>> AllEnginesAsyc()
+		public async Task<bool> CarExistsAsync(int carId)
 		{
-			throw new NotImplementedException();
+			return await repository.AllAsReadOnly<Car>().AnyAsync(c => c.Id == carId);
 		}
 
-		public Task<bool> CarExistsAsync(int carId)
+		public async Task<bool> EngineExistsAsync(int engineId)
 		{
-			throw new NotImplementedException();
+			return await repository.AllAsReadOnly<Engine>().AnyAsync(e => e.Id == engineId);
 		}
 
-		public Task<CarDetailsViewModel> DetailsAsync(int carId)
+        public async Task<int> CreateAsync(CarAddViewModel model, string ownerId)
 		{
-			throw new NotImplementedException();
+			var car = new Car()
+			{
+				Brand = model.Brand,
+				Model = model.Model,
+				Year = model.Year,
+				ImageUrl = model.ImageUrl,
+				Color = model.Color,
+				EngineId = model.EngineId,
+				PricePerDay = model.PricePerDay,
+				OwnerId = ownerId
+			};
+
+			await repository.AddAsync(car);
+			await repository.SaveChangesAsync();
+
+			return car.Id;
 		}
 
-		public Task<bool> EngineExistsAsync(int engineId)
+		public async Task<CarDetailsViewModel> DetailsAsync(int carId)
 		{
 			throw new NotImplementedException();
 		}
-	}
+    }
 }
