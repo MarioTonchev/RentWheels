dab88877b65f63d21f56ed125ae3649dc56fd1dc
MarioTonchev <mariotonchev500@gmail.com>
2024-03-12 01:20:09 +0200

    Reapply "Added unit of work pattern"
    This reverts commit c69344f0a902c15600faf4c60e120f7e9fa12fab.

---
+++ RentWheels.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace RentWheels.Infrastructure.Common
+{
+	public interface IRepository
+	{
+		IQueryable<T> All<T>() where T : class;
+		IQueryable<T> AllAsReadOnly<T>() where T : class;
+		Task AddAsync<T>(T entity) where T : class;
+		Task<int> SaveChangesAsync();
+	}
+}
