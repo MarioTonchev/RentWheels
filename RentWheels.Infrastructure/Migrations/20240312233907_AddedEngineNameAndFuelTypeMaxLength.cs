ab2c7b5e40efb7a20b2a5a489412c7bed0c5d664
MarioTonchev <mariotonchev500@gmail.com>
2024-03-13 02:37:04 +0200

    Added new property for engine

---
+++ RentWheels.Infrastructure/Migrations/20240312233907_AddedEngineNameAndFuelTypeMaxLength.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentWheels.Infrastructure.Migrations
+{
+    public partial class AddedEngineNameAndFuelTypeMaxLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FuelType",
+                table: "Engines",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                comment: "Fuel type of the engine",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldComment: "Fuel type of the engine");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Engines",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                comment: "Name of the engine");
+
+            migrationBuilder.UpdateData(
+                table: "Engines",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Small");
+
+            migrationBuilder.UpdateData(
+                table: "Engines",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Medium");
+
+            migrationBuilder.UpdateData(
+                table: "Engines",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Big");
+
+            migrationBuilder.UpdateData(
+                table: "Engines",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Sport");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Engines");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FuelType",
+                table: "Engines",
+                type: "nvarchar(max)",
+                nullable: false,
+                comment: "Fuel type of the engine",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldComment: "Fuel type of the engine");
+        }
+    }
+}
--- RentWheels.Infrastructure/Models/Engine.cs
+++ RentWheels.Infrastructure/Models/Engine.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using static RentWheels.Infrastructure.Constants.DataConstants.EngineConstants;
 
 namespace RentWheels.Infrastructure.Models
 {
@@ -10,12 +11,17 @@ namespace RentWheels.Infrastructure.Models
         [Comment("Identifier of the engine")]
         public int Id { get; set; }
         [Required]
+        [StringLength(EngineMaxNameLength)]
+        [Comment("Name of the engine")]
+        public string Name { get; set; } = string.Empty;
+        [Required]
         [Comment("Horsepower of the engine")]
         public int Horsepower { get; set; }
         [Required]
         [Comment("Cubage of the engine")]
         public int Cubage { get; set; }
         [Required]
+        [StringLength(EngineMaxFuelTypeLength)]
         [Comment("Fuel type of the engine")]
         public string FuelType { get; set; } = string.Empty;
         public IEnumerable<Car> Cars { get; set; } = new List<Car>();
