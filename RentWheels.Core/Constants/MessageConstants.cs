92e3a37e527e72a0c78a1d2ef483c554b752879f
MarioTonchev <mariotonchev500@gmail.com>
2024-03-13 02:38:38 +0200

    Added constants for error messages

---
+++ RentWheels.Core/Constants/MessageConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace RentWheels.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string RequiredMessage = "Field {0} is required";
+
+        public const string StringLengthMessage = "Field {0} must be between {2} and {1} characters long";
+
+        public const string RangeMessage = "Field {0} must be between {1} and {2}";
+    }
+}
