3592087a9654a2934819b254b4f27ba7226713d3
MarioTonchev <mariotonchev500@gmail.com>
2024-03-13 02:39:49 +0200

    implemented add action and add view

--- RentWheels/Controllers/CarController.cs
+++ RentWheels/Controllers/CarController.cs
@@ -1,12 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentWheels.Core.Contracts;
+using RentWheels.Core.VeiwModels.Car;
+using RentWheels.Infrastructure.Models;
+using System.Security.Claims;
 
 namespace RentWheels.Controllers
 {
 	public class CarController : BaseController
 	{
-		public IActionResult Index()
+		private ICarService carService;
+
+        public CarController(ICarService _carService)
+        {
+            carService = _carService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
 		{
-			return View();
+            var model = new CarAddViewModel()
+            {
+                Engines = await carService.AllEnginesFormAsync()
+            };
+
+			return View(model);
 		}
-	}
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CarAddViewModel model)
+        {
+            if (await carService.EngineExistsAsync(model.EngineId) == false)
+            {
+                ModelState.AddModelError(nameof(model.EngineId), "Selected engine does not exit!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                model.Engines = await carService.AllEnginesFormAsync();
+                return View(model);
+            }
+
+            await carService.CreateAsync(model, User.Id());
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
 }
