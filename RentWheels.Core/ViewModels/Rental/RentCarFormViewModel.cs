11f3cde78c86f298d96efd2527e929a70d308bca
MarioTonchev <mariotonchev500@gmail.com>
2024-04-03 00:51:10 +0300

    Added display names

--- RentWheels.Core/ViewModels/Rental/RentCarFormViewModel.cs
+++ RentWheels.Core/ViewModels/Rental/RentCarFormViewModel.cs
@@ -7,16 +7,20 @@ namespace RentWheels.Core.ViewModels.Rental
     public class RentCarFormViewModel
     {
         [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Start Date")]
         public string Start { get; set; } = string.Empty;
         [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "End Date")]
         public string End { get; set; } = string.Empty;
         [Required(ErrorMessage = RequiredMessage)]
         [StringLength(RentalMaxPickUpLocationLength, MinimumLength = RentalMinPickUpLocationLength
             , ErrorMessage = StringLengthMessage)]
+        [Display(Name = "Pick Up")]
         public string PickUp { get; set; } = string.Empty;
         [Required(ErrorMessage = RequiredMessage)]
         [StringLength(RentalMaxDropOffLocationLength, MinimumLength = RentalMinDropOffLocationLength
             , ErrorMessage = StringLengthMessage)]
+        [Display(Name = "Drop Off")]
         public string DropOff { get; set; } = string.Empty;
     }
 }
