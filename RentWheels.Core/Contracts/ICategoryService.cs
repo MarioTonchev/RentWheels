29249fdca5b93fca724161e5b40eb1e5055aeca8
MarioTonchev <mariotonchev500@gmail.com>
2024-04-04 20:57:39 +0300

    added new service to decouple code

---
+++ RentWheels.Core/Contracts/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using RentWheels.Core.ViewModels.Category;
+
+namespace RentWheels.Core.Contracts
+{
+	public interface ICategoryService
+	{
+		Task<bool> CategoryExistsAsync(int categoryId);
+
+		Task<IEnumerable<string>> AllCategoriesNamesAsync();
+
+		Task<IEnumerable<CategoryViewModel>> AllCategoriesFormAsync();
+	}
+}
---
+++ RentWheels.Core/Services/CategoryService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentWheels.Core.Contracts;
+using RentWheels.Core.ViewModels.Category;
+using RentWheels.Infrastructure.Common;
+using RentWheels.Infrastructure.Models;
+
+namespace RentWheels.Core.Services
+{
+	public class CategoryService : ICategoryService
+	{
+		private readonly IRepository repository;
+
+        public CategoryService(IRepository _repository)
+        {
+			repository = _repository;
+        }
+
+        public async Task<bool> CategoryExistsAsync(int categoryId)
+		{
+			return await repository.AllAsReadOnly<Category>().AnyAsync(c => c.Id == categoryId);
+		}
+
+		public async Task<IEnumerable<string>> AllCategoriesNamesAsync()
+		{
+			return await repository.AllAsReadOnly<Category>().Select(c => c.Name).ToListAsync();
+		}
+
+		public async Task<IEnumerable<CategoryViewModel>> AllCategoriesFormAsync()
+		{
+			return await repository.AllAsReadOnly<Category>().Select(e => new CategoryViewModel()
+			{
+				Id = e.Id,
+				Name = e.Name
+			}).ToListAsync();
+		}
+	}
+}
