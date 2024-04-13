d93dc8d91cead2a42d32e17c0300d172786a0c11
MarioTonchev <mariotonchev500@gmail.com>
2024-04-13 21:10:11 +0300

    Added cities to choose from in pickup and dropoff fields

--- RentWheels.Core/ViewModels/Rental/RentCarFormViewModel.cs
+++ RentWheels.Core/ViewModels/Rental/RentCarFormViewModel.cs
@@ -4,23 +4,52 @@ using static RentWheels.Infrastructure.Constants.DataConstants.RentalConstants;
 
 namespace RentWheels.Core.ViewModels.Rental
 {
-    public class RentCarFormViewModel
-    {
-        [Required(ErrorMessage = RequiredMessage)]
-        [Display(Name = "Start Date")]
-        public string Start { get; set; } = string.Empty;
-        [Required(ErrorMessage = RequiredMessage)]
-        [Display(Name = "End Date")]
-        public string End { get; set; } = string.Empty;
-        [Required(ErrorMessage = RequiredMessage)]
-        [StringLength(RentalMaxPickUpLocationLength, MinimumLength = RentalMinPickUpLocationLength
-            , ErrorMessage = StringLengthMessage)]
-        [Display(Name = "Pick Up")]
-        public string PickUp { get; set; } = string.Empty;
-        [Required(ErrorMessage = RequiredMessage)]
-        [StringLength(RentalMaxDropOffLocationLength, MinimumLength = RentalMinDropOffLocationLength
-            , ErrorMessage = StringLengthMessage)]
-        [Display(Name = "Drop Off")]
-        public string DropOff { get; set; } = string.Empty;
+	public class RentCarFormViewModel
+	{
+		[Required(ErrorMessage = RequiredMessage)]
+		[Display(Name = "Start Date")]
+		public string Start { get; set; } = string.Empty;
+		[Required(ErrorMessage = RequiredMessage)]
+		[Display(Name = "End Date")]
+		public string End { get; set; } = string.Empty;
+		[Display(Name = "Pick Up")]
+		public string PickUp { get; set; } = string.Empty;
+		[Display(Name = "Drop Off")]
+		public string DropOff { get; set; } = string.Empty;
+        public List<string> Cities { get; set; } = new List<string>()
+		{
+			"Sofia",
+			"Varna",
+			"Plovdiv",
+			"Burgas",
+			"Ruse",
+			"Stara Zagora",
+			"Pleven",
+			"Sliven",
+			"Pernik",
+			"Pazardzhik",
+			"Dobrich",
+			"Shumen",
+			"Haskovo",
+			"Veliko Tarnovo",
+			"Blagoevgrad",
+			"Yambol",
+			"Kazanlak",
+			"Asenovgrad",
+			"Kyustendil",
+			"Vratsa",
+			"Gabrovo",
+			"Targovishte",
+			"Kardzhali",
+			"Vidin",
+			"Razgrad",
+			"Svishtov",
+			"Silistra",
+			"Dupnitsa",
+			"Montana",
+			"Lovech",
+			"Dimitrovgrad",
+			"Smolyan"
+		}; 
     }
 }
