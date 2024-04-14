d75e337cf319125e81ad9e32a7c19e8184360f82
MarioTonchev <mariotonchev500@gmail.com>
2024-04-15 02:09:58 +0300

    added createdon property for message

---
+++ RentWheels.Infrastructure/Migrations/20240414214036_AddedCreatedOnPropertyForMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentWheels.Infrastructure.Migrations
+{
+    public partial class AddedCreatedOnPropertyForMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Messages",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                comment: "When the message was sent");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Messages");
+        }
+    }
+}
